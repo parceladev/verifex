1b7bae7fa2fdc6df4da02656bc3e4f5f33d2fa6e
Gerry-Latris <whateveritakes10@gmail.com>
2024-02-20 18:57:42 +0800

    Create App Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import  aboutpage from "./pages/main/aboutpage"
+import homepage from "./pages/main/homepage"
+import legitcheckpage from "./pages/main/legitcheckpage"
+
 const App = () => {
   return (
-    <div>
-      <h1 className="underline bg-black text-red-400">Hello Verifex</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<homepage/>}/>
+        <Route path="/about" element={<aboutpage/>}/>
+        <Route path="/legitcheckpage" element={<legitcheckpage/>}/>
+      </Routes>
+    </Router>
   );
 };
 
